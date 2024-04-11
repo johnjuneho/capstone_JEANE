e4188c4968ac8ec6cf7d653d4afe0bf6497727d9
Ella Crowder <ellawho4@uw.edu>
2024-04-11 12:14:07 -0700

    adding pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Home from './components/pages/home/Home';
+import Lesson1 from './components/pages/lesson-1/Lesson1';
 
 function App() {
   return (
---
+++ src/components/pages/lesson-2/Lesson2.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import HomeButton from '../../cards/HomeButton/HomeButton';
+import MenuCard from '../../cards/MenuCard/MenuCard';
+import './style.css';
+
+export default function Lesson2() {
+  return (
+    <div className="home-dashboard">
+      <div className="container">
+        <div className="container-2">
+          <div className="header">
+            <div className="logo">
+              <HomeButton />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="content">
+        <div className="menu">
+          <MenuCard />
+        </div>
+        <div className="content-card"></div>
+      </div>
+    </div>
+  );
+}
