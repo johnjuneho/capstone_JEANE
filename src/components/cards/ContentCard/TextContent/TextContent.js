ea6a2232538c86e0cf4a57c2860f2fb7fe1bd4cb
Ella Crowder <ellawho4@uw.edu>
2024-04-17 18:31:35 -0700

    removing generic homebutton css that's affecting other css

--- src/components/cards/ContentCard/TextContent/TextContent.js
+++ src/components/cards/ContentCard/TextContent/TextContent.js
@@ -12,25 +12,12 @@ export default function TextContent() {
       <div class="lesson-heading">
         <p>Objective 1: Asking Clearly</p>
         <div class="lesson-container">
-          <div class="button-container">
-            <div class="button">
-              <div class="line"></div>
-            </div>
-            <div class="button-text">Previous</div>
-          </div>
           <div class="lesson-text">
             It’s important to learn how to ask ChatGPT in a way that gets us the
             answers we need without any mix-ups or wrong information. It's like
             playing a word game with a computer—we need to pick our words super
             carefully! As we have learned, large language modules predict the
             next words. We need to think like AI and choose our words carefully!
-          </div>
-          <div class="next-button-container">
-            <div class="button-text">Next</div>
-            <div class="button">
-              <div class="line"></div>
-              <div class="line"></div>
-            </div>
           </div>
         </div>
       </div>
--- src/components/cards/HomeButton/HomeButton.js
+++ src/components/cards/HomeButton/HomeButton.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import './style.css';
 import logo from '../../images/logo.png';
-import { Link } from 'react-router-dom';
 
 export default function HomeButton() {
   return (
-    <Link to="/home">
-      <button>
-        <div>
-          <img src={logo} alt="dragon outline" />
-          <p id="logo-title">How to Train Your ChatBot</p>
-        </div>
-      </button>
-    </Link>
+    <div class="logo-container">
+      <img src={logo} alt="dragon outline" class="logo-img" />
+      <p id="logo-title">How to Train Your ChatBot</p>
+    </div>
   );
 }
